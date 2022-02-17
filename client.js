2958982c8106d9688add6b5c4fb7ad85c794312f
Richard <au.richard4@gmail.com>
2022-02-16 18:11:23 -0800

    Added Move up command with conn.write, but commented out

--- client.js
+++ client.js
@@ -11,9 +11,17 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Connection Successful");
     conn.write("Name: DIY");
+    // for (let i = 0; i < 100; i++) {
+    //   setTimeout(() => {
+    //     conn.write("Move: up");
+    //   }, 500 * i);
+    // }
   });
   conn.on("data", (data) => {
     console.log(data);
+  });
+  conn.on("moveUp", () => {
+    "Move: up";
   });
 
   //interpret incoming data as text
@@ -22,10 +30,6 @@ const connect = function () {
   return conn;
 
 };
-
-
-
-
 
 module.exports = connect;
 
